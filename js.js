fe077c7a5ed228e2c8ea09c15ad767714e5b06e1
Jelena Kocic <kocicjelena@gmail.com>
2014-05-16 12:50:15 +0200

    1

---
+++ js.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$.getJSON("json_data.json", function(data){
+$.each(data.person, function(){
+
+$("ul").append("<li>name:"+this['name']+"</li><li>age:"+this['name']+"</li>});
+});
+});
+});
